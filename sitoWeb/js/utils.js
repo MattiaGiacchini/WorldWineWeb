e50d0a600d4ec4fe582abef30928bc55b48c53b6
Daniele <dejackdani@gmail.com>
2020-12-27 10:46:17 +0100

    js - last update

--- sitoWeb/js/utils.js
+++ sitoWeb/js/utils.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+    const nav = $("body > nav");
+
+    let i = 0;
+
+    nav.hide();
+
     $("body > header > p").click(function(){
        console.log("ciao ciccio!");
+       if(i == 0){
+          nav.slideDown();
+          i = 1;
+      } else {
+          nav.slideUp();
+          i = 0;
+      }
     });
 });
