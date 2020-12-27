b7624586946939a2ec0760902608432e0dccc304
Daniele <dejackdani@gmail.com>
2020-12-27 11:16:33 +0100

    work continue

--- sitoWeb/js/utils.js
+++ sitoWeb/js/utils.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
     const nav = $("body > nav");
-
-    let i = 0;
-
     nav.hide();
 
     $("body > header > p").click(function(){
-       console.log("ciao ciccio!");
-       if(i == 0){
-          nav.slideDown();
-          i = 1;
-      } else {
-          nav.slideUp();
-          i = 0;
-      }
+       nav.slideToggle();
     });
 });
