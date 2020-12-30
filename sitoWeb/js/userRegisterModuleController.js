3281168324f5a58bf5189d3c489cc2c54b22fbcf
Daniele <dejackdani@gmail.com>
2020-12-30 17:27:43 +0100

    html - terminated register.html js - terminated animation of js

--- sitoWeb/js/userRegisterModuleController.js
+++ sitoWeb/js/userRegisterModuleController.js
@@ -1,28 +1,57 @@
 $(document).ready(function() {
-    const private = $("form > ul > li.private");
-    const business = $("form > ul > li.business");
+    // Tipologia di utente in base alla selezione
+    //const calendar = new Date();
+    //const maxYearBirthday = calendar.getFullYear() + "-" + calendar.getMonth() + "-" + calendar.getDate();
+    const psw = document.getElementById("psw");
+    const pswRepeat = document.getElementById("psw-repeat");
 
+    const private = $("form.register > ul > li.private");
+    const privateInput = $("form.register > ul > li > input.private");
+    const business = $("form.register > ul > li.business");
+    const businessInput = $("form.register > ul > li > input.business");
 
-    function(item)
-
-    function selectType(type){
-
+    // funzioni
+    function showBusiness(){
+        private.hide();
+        privateInput.attr('disabled', '');
+        business.show();
+        businessInput.removeAttr('disabled');
     }
 
+    function showPrivate(){
+       business.hide();
+       businessInput.attr('disabled', '');
+       private.show();
+       privateInput.removeAttr('disabled');
+    }
 
-    $("form > ul > li > input[type = radio]").click(selectType($(this)){
-        switch ($(this).attr("id")) {
-            case "private":
-                business.hide();
-                private.show();
-                break;
-            case "business":
-                business.show();
-                private.hide();
-                break;
-            default:
-                console.log("wrong attribute");
+    function resetCheck(){
+        if(pswRepeat.value == "") {
+            if(pswRepeat.classList.contains("unmatch")) { pswRepeat.classList.remove("unmatch"); }
+            if(pswRepeat.classList.contains("match")) { pswRepeat.classList.remove("match"); }
         }
+    }
 
-    });
+    function checkPasswordRepeat(){
+        if(psw.value == pswRepeat.value) {
+            if(pswRepeat.classList.contains("unmatch")) { pswRepeat.classList.remove("unmatch"); }
+            pswRepeat.classList.add("match");
+        } else {
+            if(pswRepeat.value != "") {
+                if(pswRepeat.classList.contains("match")) { pswRepeat.classList.remove("match"); }
+                pswRepeat.classList.add("unmatch");
+            }
+        }
+    }
+
+    // aggiunta di eventi
+    document.getElementById("private").addEventListener("click",showPrivate);
+    document.getElementById("business").addEventListener("click", showBusiness);
+    pswRepeat.addEventListener("keyup", checkPasswordRepeat);
+    psw.addEventListener("keyup", checkPasswordRepeat);
+
+    // predisposizione documento
+    $("form.register > ul > li > input").attr('required', '');
+    document.getElementById("private").setAttribute("checked", "");
+    showPrivate();
 });
