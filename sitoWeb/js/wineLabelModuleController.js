dbc3c8327e3da2035888562d1d18728271aa1d6a
Daniele <dejackdani@gmail.com>
2021-01-02 18:12:28 +0100

    html - completated form new label js - completated controll

--- sitoWeb/js/wineLabelModuleController.js
+++ sitoWeb/js/wineLabelModuleController.js
@@ -1,23 +1,134 @@
+
+function getOutLi(elements) {
+    elements.slideUp();
+    elements.attr("disabled", true);
+    elements.find("*").attr("disabled", true);
+}
+function getInLi(elements) {
+    elements.removeAttr("disabled");
+    elements.find("*").removeAttr("disabled");
+    elements.slideDown();
+}
+
+function getOutLiInstant(elements) {
+    elements.hide();
+    elements.attr("disabled", true);
+    elements.find("*").attr("disabled", true);
+}
+function getInLiInstant(elements) {
+    elements.removeAttr("disabled");
+    elements.find("*").removeAttr("disabled");
+    elements.show();
+}
+
 $(document).ready(function(){
+    const idBox       = $("form.newWineLabel .id");
     const wineElement = $("form.newWineLabel .vino");
+    const newCantina  = $("form.newWineLabel .newCantina");
+    const newVitigno  = $("form.newWineLabel .newVitigno");
     const spumElement = $("form.newWineLabel .spumante");
+    const doFull      = $("form.newWineLabel .doFull");
+    const varietale   = $("form.newWineLabel .varietale");
+    const noIg        = $("form.newWineLabel .noIg");
 
-    document.getElementById("vino").setAttribute("checked", "");
-    wineElement.show();
-    spumElement.hide();
-    $("form.newWineLabel > ul > li > fieldset >input[name = categoria]").click(function(){
-        if(document.forms["newWineLabel"]["categoria"].value == "vino"){
-            spumElement.hide();
-            spumElement.attr("disabled");
-            wineElement.show();
-            wineElement.removeAttr("disabled");
+    let lastCatSelected = document.forms["newWineLabel"]["categoria"].value;
+
+    function changeForm() {
+        let actualCatSelected = document.forms["newWineLabel"]["categoria"].value;
+        let actualIgSelection = document.forms["newWineLabel"]["ig"].value;
+        let actualClassificat = document.forms["newWineLabel"]["classificazione"].value;
+
+        // si tratta di un vino?
+        let wine = actualCatSelected == "vino";
+
+        // verifica se la cantina è da inserire nuova
+        if(document.forms["newWineLabel"]["cantina"].value == "new"){
+            getInLi(newCantina);
         } else {
-            wineElement.hide();
-            wineElement.attr("disabled");
-            spumElement.show();
-            spumElement.removeAttr("disabled");
+            getOutLi(newCantina);
         }
-        document.forms["newWineLabel"]["zucchero"].value = "";
-    });
 
+        // Verifica se è presente o meno l'identificativo dell'Etichetta
+        if(document.forms["newWineLabel"]["id"].value == "") {
+            getOutLiInstant(idBox);
+        } else {
+            getInLiInstant(idBox);
+        }
+
+        // verifica se si tratta di un vino o di uno spumante
+        if(lastCatSelected != actualCatSelected) {
+            lastCatSelected = actualCatSelected;
+            if(actualCatSelected == "vino"){
+                getInLi(wineElement);
+                getOutLi(spumElement);
+            } else {
+                getInLi(spumElement);
+                getOutLi(wineElement);
+            }
+            document.forms["newWineLabel"]["zucchero"].value = "";
+        }
+
+        if(wine) {
+            // verifica se si tratta di un vino con indicazioneGeografica
+            if(actualIgSelection == "presente") {
+                getInLi(doFull);
+                getOutLi(noIg);
+            } else {
+                getInLi(noIg);
+                getOutLi(doFull);
+            }
+
+            // se non possiede indicazioneGeografica
+            if(actualIgSelection == "presente" || actualClassificat == "varietale") {
+                getInLi(varietale);
+            } else {
+                getOutLi(varietale);
+            }
+
+            // verifica se è da inserire un nuovo vino
+            if(document.forms["newWineLabel"]["vitigno"].value == "new"
+               && (actualIgSelection == "presente"
+                   || actualClassificat == "varietale")){
+                getInLi(newVitigno);
+            } else {
+                getOutLi(newVitigno);
+            }
+        } else {
+            getOutLi(noIg);
+            getOutLi(doFull);
+            getOutLi(varietale);
+            getOutLi(newVitigno);
+        }
+    }
+
+    /**
+    *   selezione vino oppure spumante
+    */
+    $("form.newWineLabel input[name = categoria]").on("change", changeForm);
+    //document.getElementById("categoria").addEventListener("change", changeForm);
+
+    /**
+    *   Aggiungi o meno nuova cantina
+    */
+    document.getElementById("cantina").addEventListener("change", changeForm);
+
+    /**
+    *   Indicazione geografica presente o meno
+    */
+    $("form.newWineLabel input[name = ig]").on("change", changeForm);
+
+    /**
+    *   Vino Generico oppure varietale
+    */
+    $("form.newWineLabel input[name = classificazione]").on("change", changeForm);
+
+    /**
+    *   apparizione o scoparsa del vitigno da aggiungere
+    */
+    document.getElementById("vitigno").addEventListener("change", changeForm);
+
+
+    document.forms["newWineLabel"]["categoria"].value = "vino";
+    document.forms["newWineLabel"]["classificazione"].value = "generico";
+    changeForm();
 });
