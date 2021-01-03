4c2dd2e3a0c7324ff8fe1f76f03692461569b9fb
Mattia Giacchini <ma.giacchini99@gmail.com>
2021-01-03 01:20:20 +0100

    JS - cart update

---
+++ sitoWeb/js/cart.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    const articleLocation = $("section.articoli article.tile");
+    let cartValue = 0.00;
+    let quantity = 0;
+    let price = 0;
+
+    $(document).on("click touchend", ".quantityInput", calculateCartValue);
+    window.onload = calculateCartValue();
+
+    function updateCart(){
+        $("p#cartValue").text(cartValue + " €");
+    }
+
+    function calculateCartValue(){
+        cartValue = 0;
+        articleLocation.each(function(){
+            quantity = $(this).find("input").val();
+            price = $(this).find("p.tileImportantInfo").html();
+            price = price.replace("€", "");
+            cartValue = cartValue + (quantity * parseFloat(price));
+        });
+
+        console.log(cartValue);
+        updateCart();
+
+        return;
+    }
+
+});
