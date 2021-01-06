46d11cab86742ba26a90346ec9e6310990895613
Daniele <dejackdani@gmail.com>
2021-01-06 10:47:48 +0100

    fixed visualizzation of wineCard

--- sitoWeb/js/wineCard.js
+++ sitoWeb/js/wineCard.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    let img = $("body > main > div.article-container > article.wineCard > div > div > img");
-    let i = 30;
-
-    //img.css("background", "linear-gradient(to right, #B9653D 50%, #C4C4C4 50%)");
-    //img.css("background", "linear-gradient(to right, #B9653D " + 10 + "%, #C4C4C4 " + 10 + "%)");
-    img.each(function(){
-        $(this).css("background", "linear-gradient(to right, #B9653D " + i + "%, #C4C4C4 " + i + "%)");
-        i += 30;
+    let vote;
+    $("article.wineCard img[alt^=\"voto:\"]").each(function(){
+        vote = $(this).attr("alt")
+        vote = vote.slice(5, vote.length-4);
+        vote = parseFloat(vote);
+        vote = vote / 5.0 * 100;
+        $(this).css("background", "linear-gradient(to right, #B9653D " + vote + "%, #C4C4C4 " + vote + "%)");
     });
-
 });
--- sitoWeb/js/wineDetails.js
+++ sitoWeb/js/wineDetails.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-    
+
     const time = 1000;
     const buttons = $("div.details button");
     const nextElement = buttons.next();
     let element;
-    let vote;
-    let img;
 
     buttons.click(function(e){
         if($(this).hasClass("selected")){  // lo nascondo
@@ -19,14 +17,6 @@ $(document).ready(function(){
                    .next()
                    .slideDown(time);
         }
-    });
-
-    $("article img[alt^=\"voto:\"]").each(function(){
-        vote = $(this).attr("alt")
-        vote = vote.slice(5, vote.length-4);
-        vote = parseFloat(vote);
-        vote = vote / 5.0 * 100;
-        $(this).css("background", "linear-gradient(to right, #B9653D " + vote + "%, #C4C4C4 " + vote + "%)");
     });
 
     buttons.next().hide();
