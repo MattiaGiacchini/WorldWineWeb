584a0b9cbd3d33a0458a8cc09d4f216427fc0400
Mattia Giacchini <ma.giacchini99@gmail.com>
2021-01-10 13:55:02 +0100

    PHP - created warehouse movement tile

---
+++ sitoWeb/template/tileMagazzino.php
@@ -0,0 +1,9 @@
+<article class="tile magazzino">
+    <div class="tileContent">
+        <div class="tileBody">
+            <h4><?php echo $warehouseLoad["date"]; ?> - <?php echo $warehouseLoad["time"]; ?></h4>
+            <p><?php echo $warehouseLoad["description"]; ?></p>
+        </div>
+    </div>
+    <p><?php echo $warehouseLoad["amount"]; ?></p>
+</article>
