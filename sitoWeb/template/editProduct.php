0a8a0d03bb94e05a886b3473da45e6132300eaa8
Daniele <dejackdani@gmail.com>
2021-01-19 19:48:34 +0100

    php - fixed html editProduct form

--- sitoWeb/template/editProduct.php
+++ sitoWeb/template/editProduct.php
@@ -4,15 +4,18 @@
     </div>
 </div>
 <div class="link">
-    <button type="button" name="button" onclick="document.location='./newWineLabel.html'">Modifica Etichetta</button>
+    <button type="button" name="button" onclick="document.location='./newWineLabel.html?idEtichetta=<?php echo $_GET["idEtichetta"]; ?>'">Visualizza Etichetta</button>
 </div>
-<form name="editProduct" class="editProduct" action="#" method="POST">
+<form name="editProduct" class="editProduct" action="./insertUpdateProduct.php" method="POST" enctype="multipart/form-data">
     <ul>
-        <li class="update">
+        <li>
             <label for="id">Contenitori Abbinati</label>
             <select class="id" name="id" id="id">
-                <option value="">Seleziona per i dettagli...</option>
-                <?php if(count($templateParams["visible"]) !=0 ) { ?>
+                <option value="">Seleziona il contenitore...</option><?php
+                if(count($templateParams["unsetted"]) != 0) { ?>
+                <option value="new">Aggiungi Nuovo Abbinamento...</option>
+                <?php }
+                if(count($templateParams["visible"]) !=0 ) { ?>
                 <optgroup label="visibili a catalogo">
                 <?php   foreach ($templateParams["visible"] as $container) { ?>
                     <option value="<?php echo $container["idContenitore"] ?>"><?php echo sprintf('%2.3f',$container["capacitaContenitore"])."L - ".$container["nomeContenitore"]; ?></option>
@@ -21,11 +24,22 @@
                 <?php }
                 if(count($templateParams["invisible"]) !=0 ) {?>
                 <optgroup label="non visibili a catalogo">
-                <?php foreach ($templateParams["visible"] as $container) { ?>
+                <?php foreach ($templateParams["invisible"] as $container) { ?>
                     <option value="<?php echo $container["idContenitore"] ?>"><?php echo sprintf('%2.3f',$container["capacitaContenitore"])."L - ".$container["nomeContenitore"]; ?></option>
                 <?php } ?>
                 </optgroup>
                 <?php } ?>
+            </select>
+        </li>
+        <li class="newContainer">
+            <label for="idNewContainer">Contenitori non ancora Abbinati</label>
+            <select class="idNewContainer" name="idNewContainer" id="idNewContainer">
+                <option value="">Seleziona il contenitore...</option>
+                <?php if(count($templateParams["unsetted"]) !=0 ) {
+                        foreach ($templateParams["unsetted"] as $container) { ?>
+                <option value="<?php echo $container["idContenitore"] ?>"><?php echo sprintf('%06.3f', $container["capacitaContenitore"])."L - ".$container["nomeContenitore"]; ?></option>
+                <?php   }
+                } ?>
             </select>
         </li>
         <li class="update">
@@ -37,51 +51,21 @@
                 <input type="number" name="price" id="price" value="" min="0.00" step="0.01">
                 <label for="iva">Imponibile %</label>
                 <input type="number" name="iva" id="iva" value="" max="100.00" min="0.00" step="0.01">
-                <label for="photo">Foto del Prodotto</label>
-                <input type="file" id="photo" name="photo" accept="image/*"/>
+                <label for="newPhoto">Carica nuova foto</label>
+                <input type="file" id="newPhoto" name="newPhoto" accept=".gif,.jpg,.jpeg,.png,"/>
             </fieldset>
         </li>
         <li class="update">
-            <input type="submit" value="Aggiorna">
+            <div class="figure">
+                <figure>
+                    <figcaption>Immagine attualmente in uso</figcaption>
+                    <img src="<?php echo getWineImgURL(0,0); ?>" id="photo" alt="Immagine di default">
+                </figure>
+            </div>
         </li>
-        <li>
-            <button type="button" name="button">Aggiungi Nuovo Abbinamento</button>
-        </li>
-    </ul>
-</form>
-<form name="addNewProduct" class="addNewProduct" action="#" method="POST">
-    <ul>
-        <li>
-            <p>Contenitore da Abbinare</p>
-        <li>
-        <li>
-            <label for="idNewContainer">Contenitori non ancora Abbinati</label>
-            <select class="idNewContainer" name="idNewContainer" id="idNewContainer">
-                <option value="">Seleziona il contenitore...</option>
-                <?php if(count($templateParams["unsetted"]) !=0 ) { ?>
-                <optgroup label="visibili a catalogo">
-                <?php   foreach ($templateParams["unsetted"] as $container) { ?>
-                    <option value="<?php echo $container["idContenitore"] ?>"><?php echo sprintf('%06.3f', $container["capacitaContenitore"])."L - ".$container["nomeContenitore"]; ?></option>
-                <?php   } ?>
-                </optgroup>
-            <?php } ?>
-            </select>
-        </li>
-        <li>
-            <fieldset class="modify">
-                <p>Dati di Abbinamento</p>
-                <label for="visibleNewContainer">Visibile a catalogo</label>
-                <input type="checkbox" name="visibleNewContainer" id="visibleNewContainer" value="true">
-                <label for="priceNewContainer">Prezzo €</label>
-                <input type="number" name="priceNewContainer" id="priceNewContainer" value="" min="0.00" step="0.01">
-                <label for="ivaNewContainer">Imponibile (iva %)</label>
-                <input type="number" name="ivaNewContainer" id="ivaNewContainer" value="" max="100.00" min="0.00" step="0.01">
-                <label for="photoNewContainer">Foto del Prodotto</label>
-                <input type="file" id="photoNewContainer" name="photoNewContainer" accept="image/*"/>
-            </fieldset>
-        </li>
-        <li>
-            <input type="submit" value="Abbina">
+            <input type="hidden" name="idLabel" value="<?php echo $_GET["idEtichetta"]; ?>">
+        <li class="update">
+            <input type="submit" value="inserisci">
         </li>
     </ul>
 </form>
