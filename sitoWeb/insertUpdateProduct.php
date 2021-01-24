12dd7816030d88cc0b79eef1215c449e1ea1559d
Daniele <dejackdani@gmail.com>
2021-01-25 00:33:58 +0100

    try last fix

--- sitoWeb/insertUpdateProduct.php
+++ sitoWeb/insertUpdateProduct.php
@@ -31,6 +31,9 @@
                     if($result == 0){
                         callBackWithError($_POST["idLabel"], $msg);
                     }
+                    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+                    header("Cache-Control: no-cache");
+                    header("Pragma: no-cache");
                 }
 
                 header("Location:./editProduct.php?idEtichetta=".$_POST["idLabel"]);
@@ -57,6 +60,9 @@
                     if($result == 0) {
                         callBackWithError($_POST["idLabel"], $msg);
                     }
+                    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+                    header("Cache-Control: no-cache");
+                    header("Pragma: no-cache");
                 }
 
                 header("Location:./editProduct.php?idEtichetta=".$_POST["idLabel"]);
